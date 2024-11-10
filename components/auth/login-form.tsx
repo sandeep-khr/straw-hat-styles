24c3530942566b7dff1339410e04c8a4a32d1649
Sandeep <sandeepkharoundha@gmail.com>
2024-11-10 22:59:11 +0530

    Styled login page

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -5,13 +5,13 @@ import {AuthCard} from "./auth-card"
 export const LoginForm = () => {
   return (
     <AuthCard
-      cardTitle="Wlcome Back!"
+      cardTitle="Welcome Back!"
       backButtonHref="/auth/register"
       backButtonLabel="Create a new account"
       showSocials
     >
       <div>
-        <h1>Hey</h1>
+        
       </div>
     </AuthCard>
   )
--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -1,16 +1,21 @@
 import {auth} from "@/server/auth"
 import { UserButton } from "./user-button";
 import Link from "next/link";
+import Image from "next/image";
 
 export default async function Nav(){
     const session = await auth();
     console.log(session);
 
     return(
-        <header className="bg-slate-500 p-4">
+        <header className="py-8">
             <nav>
                 <ul className="flex justify-between">
-                    <li>Logo</li>
+                    <li>
+                        <Link href={"/"}>
+                        <Image src="/straw-hat.png" alt="Logo" width={75} height={30} priority/>
+                        </Link>
+                    </li>
                     <li>
                         {!session ? (
                             <button><Link aria-label="sign-in" href={"/auth/login"}>login</Link></button>
@@ -21,3 +26,4 @@ export default async function Nav(){
         </header>
     )
 }
+
